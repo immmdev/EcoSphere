6693d2275a6718ccdfc255c74835f6740d5f0ad8
darshitachaurasia <161128989+darshitachaurasia@users.noreply.github.com>
2025-07-05 14:32:48 +0530

    nav layout and authContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Nav from './components/Nav'
 
 function App() {
 
   return (
     <>
+    <Nav/>
       <h1 className='text-red-500'>EcoSphere</h1>
       <Routes>
         <Route path='/' element={<Home />} />
