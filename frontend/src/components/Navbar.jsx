c84fac477d77477bd211f8ac589c45180071e7c8
Dev Singh <myselfdevsingh123@gmail.com>
2025-07-05 19:43:08 +0530

    fixed styling problem in nav and structure completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,12 +6,12 @@ function Navbar() {
 
   return (
     <nav className="bg-white border-b border-gray-300 px-4 py-3">
-      <div className="max-w-7xl mx-auto flex items-center justify-between">
+      <div className="max-w-7xl mx-auto flex  items-center justify-between">
 
         {/* Logo */}
         <Link to="/" className="flex items-center gap-2">
           <img src="/earth.png" alt="logo" className="w-10 h-10" />
-          <span className="text-darkCharcoal text-lg font-semibold">EcoSphere</span>
+          <span className="text-gray-600 text-lg font-semibold">EcoSphere</span>
         </Link>
 
         {/* Toggler for Mobile */}
@@ -46,19 +46,20 @@ function Navbar() {
 
         {/* Nav Links */}
         <div className={`${isOpen ? 'block' : 'hidden'} lg:flex items-center gap-6`}>
-          <ul className="flex flex-col lg:flex-row items-center gap-4 text-sm mt-4 lg:mt-0">
-            <li>
-              <li>
-                <Link to="/eco-shop" className="text-gray-500 hover:text-caribbeanGreen transition-colors duration-200 no-underline">
-                  EcoShop
-                </Link>
-              </li>
-            </li>
+          <ul className="flex flex-col lg:flex-row items-center gap-10 text-sm mt-4 lg:mt-0">
+
             <li>
               <Link to="/" className="text-gray-500 hover:text-caribbeanGreen transition-colors duration-200 no-underline">
                 Home
               </Link>
             </li>
+
+            <li>
+              <Link to="/eco-shop" className="text-gray-500 hover:text-caribbeanGreen transition-colors duration-200 no-underline">
+                EcoShoping
+              </Link>
+            </li>
+
             <li>
               <Link to="/communities" className="text-gray-500 hover:text-caribbeanGreen transition-colors duration-200 no-underline">
                 Communities
