bf2466e194f007cfd0de8c6f29111de33c3e64e4
Pranshu Shakya <pranshushakya2027@gmail.com>
2025-07-06 21:45:10 +0530

    Functionality for user login and signup added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,29 @@
-
-import { useState } from 'react'
-import Initiative from "./pages/Initiative"
-import { Routes, Route } from 'react-router-dom';
-import { Home, Login, Signup } from './pages';
-import Profile from './pages/Profile';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Communities from './pages/Communities';
-import Initiatives from './pages/Initiative';
-import Support from './pages/Support';
-import Learn from './pages/Learn';
-import Contact from './pages/Contact';
-import EcoShop from './pages/EcoShop';
+import { Routes, Route } from "react-router-dom";
+import { Home, Login, Signup, Support, Learn, Contact, Initiative, EcoShop, Profile, Communities } from "./pages";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/Initiative' element={<Initiative />} />
-
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/support' element={<Support />} />
-        <Route path='/learn' element={<Learn />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/initiatives' element={<Initiatives />} />
-        <Route path='/eco-shop' element={<EcoShop />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/communities' element={<Communities />} />
-
-      </Routes>
-      <Footer />
-    </div>
-  );
+	return (
+		<div>
+			<ToastContainer />
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/support" element={<Support />} />
+				<Route path="/learn" element={<Learn />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/initiatives" element={<Initiative />} />
+				<Route path="/eco-shop" element={<EcoShop />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/communities" element={<Communities />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
